07dc1927414a223c33ffe717738947fb23ea7a92
Ypriom121 <pritomgupta121@gmail.com>
2023-11-24 17:37:03 +0600

    login and signup page set up done

--- src/SignUpPage/Login.jsx
+++ src/SignUpPage/Login.jsx
@@ -1,10 +1,53 @@
+import { Link } from "react-router-dom";
 
 
 const Login = () => {
+ const handleLogin =(e)=>{
+ e.preventDefault()
+ }
 return (
-  <div>
-     login                                                                                     
+<div className="hero min-h-screen bg-base-200">
+  <div className="hero-content flex-col lg:flex-row">
+    <div className=" w-1/2 mr-6">
+   {/* <Lottie animationData={login}></Lottie> */}
+    </div>
+    <div className="card flex-shrink-0 w-full max-w-md shadow-2xl 
+    bg-gradient-to-r from-[#383838] to-[#636262] text-white">
+          <div className=" w-1/2 mr-6">
+   {/* <Lottie animationData={login}></Lottie> */}
+    </div>
+      <form onSubmit={handleLogin} className="card-body">
+      <h1 className="text-2xl font-bold">Login!</h1>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text text-white">Email</span>
+          </label>
+          <input type="email" placeholder="email" name='email' className="input text-black input-bordered" required />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text text-white">Password</span>
+          </label>
+          <input type="password" placeholder="password" name='password' className="input text-black input-bordered" required />
+          <label className="label">
+            <a href="#" className="label-text-alt link link-hover text-white">Forgot password?</a>
+          </label>
+        </div>
+        <div className="form-control mt-6">
+          <button className="btn btn-primary">Login</button>
+        </div>
+        <p className='text-center'>New to our website ?  
+        <Link className='text-cyan-400 font-medium' to='/signUp'>
+        Sign Up
+        </Link></p>
+      </form>
+      <div className="py-4 text-center">
+       <p className="text-center text-lg text-cyan-400">or connect with</p>
+       <button className="btn-sm rounded-lg  btn-warning">Google login</button>
+      </div>
+    </div>
   </div>
+</div>
 );
 };
 
--- src/SignUpPage/SignUp.jsx
+++ src/SignUpPage/SignUp.jsx
@@ -1,10 +1,64 @@
+import { Link } from "react-router-dom";
 
 
 const SignUp = () => {
+  const handleSignUp =(e)=>{
+    e.preventDefault()
+  }
 return (
-<div>
-  signup                                                                                        
- </div>
+<div className="hero min-h-screen bg-base-200">
+  <div className="hero-content flex-col lg:flex-row">
+    <div className=" w-1/2 mr-6">
+    {/* <img src={img} alt="" /> */}
+    {/* <Lottie animationData={animation}></Lottie> */}
+    </div>
+    <div className="card flex-shrink-0 w-full max-w-sm 
+    bg-gradient-to-r from-[#383838] to-[#63626232] text-white">
+      <form onSubmit={ handleSignUp } className="card-body">
+      <h1 className="text-2xl font-bold">Sign Up</h1>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text text-white">Name</span>
+          </label>
+          <input type="Name" placeholder="Name" name='Name' className="input text-black input-bordered" required />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text text-white">Email</span>
+          </label>
+          <input type="email" placeholder="email" name='email' className="input text-black input-bordered" required />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text text-white">Password</span>
+          </label>
+          <input type="password" placeholder="password" name='password' className="input text-black input-bordered" required />
+          <label className="label">
+            <a href="#" className="label-text-alt link link-hover text-white">Forgot password?</a>
+          </label>
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text text-white">Photo</span>
+          </label>
+          <input type="photo" placeholder="photo url" name='photo' className="input text-black input-bordered" required />
+          <label className="label">
+            <a href="#" className="label-text-alt link link-hover text-white">Forgot password?</a>
+          </label>
+        </div>
+      
+              
+        <div className="form-control mt-6">
+          <button className="btn btn-primary">Sign up</button>
+        </div>
+        <p className='text-center'>Already Have An Account? <Link
+         className='text-blue-500' to='/login'>
+        Login
+        </Link></p>
+      </form>
+    </div>
+  </div>
+</div>
    );
 };
 
