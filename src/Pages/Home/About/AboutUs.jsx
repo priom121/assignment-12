885fb29e2a3bf30494b28f97c7369a992ce7ff90
Ypriom121 <pritomgupta121@gmail.com>
2023-11-29 01:51:25 +0600

    about us section done

--- src/Pages/Home/About/AboutUs.jsx
+++ src/Pages/Home/About/AboutUs.jsx
@@ -3,7 +3,31 @@
 const AboutUs = () => {
 return (
  <div>
-     about us                                                                                     
+  <h3 className="text-4xl font-bold text-center mt-12 text-red-500">About Us  </h3>
+  <div className="divider"></div>
+          <h1 className="text-2xl text-center  font-semibold">Our Global Domains</h1>
+          
+          <div className="flex justify-evenly mt-5">
+          <h1 className="text-xl font-medium">Bangladesi matrimony</h1> 
+          <h1 className="text-xl font-medium">Nepal matrimony</h1> 
+          <h1 className="text-xl font-medium">Maldip matrimony</h1>  
+            </div> 
+            <div className="flex justify-evenly gap-5 rounded shadow-2xl shadow-red-300">
+              <p style={{border:'2px solid crimson'}} className="flex-1 ">
+              About BangladeshiMatrimony
+BangladeshiMatrimony.com is <br/> the No.1 most trusted matrimony .<br/> site for Bangladeshi brides and grooms .<br/> Lakhs of members have successfully found <br/> their life partners here!.<br/> Browse through our vast selection of profiles from all major sects such as Sunnis, Shia, Deobandi, Barelvi, Dawoodi Bohra,.<br/> etc. You can also find profiles that belong to Muslim,.<br/> Buddhist, Hindu, Christian and other communities. Register for FREE and find your.<br/> perfect soulmate on BangladeshiMatrimony... 
+              </p>
+              <p style={{border:'2px solid red'}} className="flex-1">
+              About SrilankanMatrimony
+SrilankanMatrimony.com is part of Globalmatrimony. Register with us for FREE to find a partner from your own community. Take advantage of our user friendly search features to find a bride or groom who matches your preferences. Join us and begin your happy journey today.. 
+              </p>
+              <p style={{border:'2px solid green'}} className="flex-1">
+             Maldip SrilankanMatrimony
+Maldip Matrimony.com is part of Globalmatrimony. Register with us for FREE to find a partner from your own community. Take advantage of our user friendly search features to find a bride or groom who matches your preferences. Join us and begin your happy journey today.. 
+              </p>
+              
+              
+              </div>                                                                   
   </div>
 );
 };
